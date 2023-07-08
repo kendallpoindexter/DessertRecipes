862fb09a69da015ed70e8d7ee38f863b0ea3fcc6
kendall poindexter <kendall.c.poindexter@gmail.com>
2023-07-08 18:31:06 -0400

    initial project setup

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Kendall Poindexter on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
