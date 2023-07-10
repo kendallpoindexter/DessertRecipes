3bf4eb8377e58e1129e4d761116378a97d200ce4
kendall poindexter <kendall.c.poindexter@gmail.com>
2023-07-10 19:21:05 -0400

    Add home list to navigation stack

--- DessertRecipes/DessertRecipesApp.swift
+++ DessertRecipes/DessertRecipesApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct DessertRecipesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                HomeListView()
+            }
         }
     }
 }
